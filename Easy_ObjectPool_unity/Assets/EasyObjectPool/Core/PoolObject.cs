f2e6520f7fc5671f66e85da0fb572a30e6410090
WislonLin <p96091123@gs.ncku.edu.tw>
2021-03-19 18:27:20 +0800

    Version_01

---
+++ Easy_ObjectPool_unity/Assets/EasyObjectPool/Core/PoolObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MarchingBytes
+{
+	public class PoolObject : MonoBehaviour
+    {
+		public string poolName;
+		//defines whether the object is waiting in pool or is in use
+		public bool isPooled;
+	}
+}
